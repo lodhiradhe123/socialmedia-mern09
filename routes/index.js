3e877c15037cc1a5479b0e0160700c160f0c8f5c
master-dhanesh <dhanesh1296@gmail.com>
2024-04-30 20:32:55 +0530

    login register home about page anf get routes created

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+    res.render("index");
+});
+
+router.get("/login", function (req, res, next) {
+    res.render("login");
+});
+
+router.get("/register", function (req, res, next) {
+    res.render("register");
+});
+
+router.get("/about", function (req, res, next) {
+    res.render("about");
 });
 
 module.exports = router;
