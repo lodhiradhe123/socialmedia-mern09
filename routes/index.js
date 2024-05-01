9d73db9d7a82c554a1729e61414d54eefbfd7316
master-dhanesh <dhanesh1296@gmail.com>
2024-05-01 20:25:36 +0530

    db connected and user schema made

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
+const User = require("../models/userSchema");
+
 router.get("/", function (req, res, next) {
     res.render("index");
 });
@@ -11,6 +13,16 @@ router.get("/login", function (req, res, next) {
 
 router.get("/register", function (req, res, next) {
     res.render("register");
+});
+
+router.post("/register-user", async function (req, res, next) {
+    try {
+        const newUser = new User(req.body);
+        await newUser.save();
+        res.redirect("/login");
+    } catch (error) {
+        res.send(error);
+    }
 });
 
 router.get("/about", function (req, res, next) {
