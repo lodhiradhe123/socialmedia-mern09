03f66f80bde57f305ee66473ffba9a0d90d06084
master-dhanesh <dhanesh1296@gmail.com>
2024-06-04 19:57:23 +0530

    online db connected

--- models/connect.js
+++ models/connect.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
 mongoose
-    .connect("mongodb://127.0.0.1:27017/socialmedia09")
+    // .connect("mongodb://127.0.0.1:27017/socialmedia09")
+    .connect(
+        "mongodb+srv://dhanesh-malviya:dhanesh123@mastercluster.i7cpa.mongodb.net/socialmedia09?retryWrites=true&w=majority&appName=MasterCluster"
+    )
     .then(() => console.log("db connected!"))
     .catch((err) => console.log(err.message));
